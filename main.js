f0f5da4a915b135a485db13987e02c888caee3d8
Tanisha704 <76688253+Tanisha704@users.noreply.github.com>
2022-03-23 15:45:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 song1=""
 song2=""
-
+left_wrist_x=0
+left_wrist_y=0
+right_wrist_x=0
+right_wrist_y=0
+score_leftwrist=0
 function preload(){                 
 song1=loadSound('music.mp3')
 song2=loadSound('music.mp3')
@@ -8,12 +12,41 @@ song2=loadSound('music.mp3')
 function setup(){
 canvas=createCanvas(600,600)
 canvas.center()
-camera=createCapture(VIDEO)
-camera.hide()
+camera1=createCapture(VIDEO)
+camera1.hide()
+poseNet=ml5.poseNet(camera1,modelLoaded)
+poseNet.on("pose",gotPoses)
 }
-function draw(){
-image(camera,0,0,600,600)
+function modelLoaded(){
+    console.log("posenet is connected")
 }
+
+
+
 function play(){
     song.play()
+}
+function gotPoses(results){
+    if (results.length>0) {
+        console.log(results)
+        left_wrist_x=results[0].pose.leftWrist.x;
+        left_wrist_y=results[0].pose.leftWrist.y;
+        right_wrist_x=results[0].pose.rightWrist.x;
+        right_wrist_y=results[0].pose.rightWrist.y;
+        score_leftwrist=results[0].pose.keypoints[9].score;
+    }
+   
+}
+
+function draw(){
+    image(camera1,0,0,600,600)
+    if (score_leftwrist>0.2) {
+        circle(left_wrist_x,left_wrist_y,20)
+      
+     new_leftWrist_y=Number(left_wrist_y)
+     new_leftWrist_y=floor(new_leftWrist_y)
+     new_leftWrist_y=new_leftWrist_y/500
+
+     
+    }
 }
