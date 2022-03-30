2acce53080a4c3495762d375fccd76dc6b9eadc7
Tanisha704 <76688253+Tanisha704@users.noreply.github.com>
2022-03-30 15:42:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ left_wrist_y=0
 right_wrist_x=0
 right_wrist_y=0
 score_leftwrist=0
+score_rightwrist=0
 function preload(){                 
 song1=loadSound('music.mp3')
 song2=loadSound('music2.mp3')
@@ -34,6 +35,7 @@ function gotPoses(results){
         right_wrist_x=results[0].pose.rightWrist.x;
         right_wrist_y=results[0].pose.rightWrist.y;
         score_leftwrist=results[0].pose.keypoints[9].score;
+        score_rightwrist=results[0].pose.keypoints[10].score;
     }
    
 }
@@ -46,8 +48,21 @@ function draw(){
         song1.stop()
       if (song2_status==false) {
        song2.play()
-       document.getElementById("song").innerHTML="playing Peterpan song "
+       document.getElementById("song").innerHTML="playing Peterpan Theme song "
       }
     
 }
+
+song1_status = song1.isPlaying();
+if (score_rightwrist>0.2) {
+    circle(right_wrist_x,right_wrist_y,20)
+    song2.stop()
+  if (song1_status==false) {
+   song1.play()
+   document.getElementById("song").innerHTML="playing Harry Potter Theme song "
+  }
+
 }
+}
+
+
